af89a85cde1cce9799d8b96f18a817fdf4551ae9
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-28 09:38:56 -0300

    Search Bar removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,6 +31,10 @@ const Navbar = () => {
                             <a className="nav-link" href="#" onClick={toggleMenu}>Songs</a>
                         </li>
                     </ul>
+                    {/* <form className="d-flex">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-primary" type="submit">Search</button>
+                    </form> */}
                 </div>
             </div>
         </nav>

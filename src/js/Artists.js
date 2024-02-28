eec1c0fe4529419840d0f9fc9f0657ca77bc7af6
gustavo <gustavo.cesariosilva15@gmail.com>
2024-02-28 11:44:57 -0300

    Artists Page Filter Genre

---
+++ src/js/Artists.js
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+const fetchToken = async (clientId, clientSecret, setToken, setError) => {
+  try {
+    const response = await axios.post("https://accounts.spotify.com/api/token", null, {
+      params: {
+        grant_type: 'client_credentials',
+        client_id: clientId,
+        client_secret: clientSecret
+      },
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      }
+    });
+
+    setToken(response.data.access_token);
+  } catch (error) {
+    setError(error);
+  }
+};
+
+const fetchGenreArtists = async (token, genre, setArtistData, setError) => {
+  try {
+    const response = await axios.get('https://api.spotify.com/v1/search', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      params: {
+        q: `genre:${genre}`,
+        type: 'artist',
+        limit: 10, 
+      },
+    });
+
+    setArtistData(response.data.artists.items);
+  } catch (error) {
+    setError(error);
+  }
+};
+
+export { fetchToken, fetchGenreArtists };
--- src/pages/Artists.jsx
+++ src/pages/Artists.jsx
@@ -3,69 +3,53 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { Grid } from '@mui/material';
 import ArtistCard from '../components/ArtistCard';
+import { fetchGenreArtists, fetchToken } from '../js/Artists';
 
-const SpotifyPlayer = () => {
+const Artists = () => {
   const [token, setToken] = useState('');
   const [artistData, setArtistData] = useState([]);
   const [error, setError] = useState(null);
+  const [selectedGenre, setSelectedGenre] = useState('Rap');
   const clientId='879496c5b323472bbd08843975309a97';
   const clientSecret='b8108264c4fa4a2d9b90d62720d065b9';
 
   useEffect(() => {
-    const fetchToken = async () => {
-      try {
-        const response = await axios.post("https://accounts.spotify.com/api/token", null, {
-          params: {
-            grant_type: 'client_credentials',
-            client_id: clientId,
-            client_secret: clientSecret
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-          }
-        });
-
-        setToken(response.data.access_token);
-      } catch (error) {
-        setError(error);
-      }
-    };
-
-    fetchToken();
+    fetchToken(clientId, clientSecret, setToken, setError);
   }, []);
 
   useEffect(() => {
     if (token) {
-      const fetchGenreArtists = async ( genre ) => {
-        try {
-          const response = await axios.get('https://api.spotify.com/v1/search', {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-            params: {
-                q: `genre:${genre}`,
-                type: 'artist',
-                limit: 10, 
-              },
-          });
-
-          setArtistData(response.data.artists.items);
-        } catch (error) {
-          setError(error);
-        }
-      };
-
-      fetchGenreArtists('pop');
+      fetchGenreArtists(token, selectedGenre, setArtistData, setError); 
     }
-  }, [token]);
+  }, [token, selectedGenre]);
+
+  const handleGenreChange = (event) => {
+    setSelectedGenre(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
   if (error) {
     return <div>Error: {error.message}</div>;
-  } else if (!token || !artistData.length < 0) {
+  } 
+  else if (!token || !artistData.length === 0) {
     return <div>Loading...</div>;
-  } else {
+  } 
+  else {
     return (
       <div>
+        <div className="navbar navbar-expand-lg" style={{right: 0}}>
+            <form className="d-flex" onSubmit={handleSubmit}>
+              <select className="form-select" style={{backgroundColor:'var(--secondary)'}} onChange={handleGenreChange} value={selectedGenre}>
+                <option value="rap">Rap</option>
+                <option value="pop">Pop</option>
+                <option value="rock">Rock</option>
+                <option value="mpb">MPB</option>
+              </select>
+            </form>
+          </div>
         <Grid container spacing={2}>  
             {artistData.map(artist => (
                 <Grid key={artist.id} item xs={12} sm={6} md={4} lg={3}>
@@ -78,4 +62,4 @@ const SpotifyPlayer = () => {
   }
 };
 
-export default SpotifyPlayer;
+export default Artists;
